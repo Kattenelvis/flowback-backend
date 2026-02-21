f4e2ce34de3981aabacbc1e2afcad23d19764b18
emil <53380638+Kattenelvis@users.noreply.github.com>
2026-02-21 15:02:48 +0100

    Actions taken by admin fix for thread

--- flowback/server/selectors.py
+++ flowback/server/selectors.py
@@ -2,7 +2,8 @@ from rest_framework.exceptions import PermissionDenied
 
 from flowback.user.models import User, Report
 from flowback.poll.models import Poll
-from django.db.models import Exists, OuterRef, Value, Case, When
+from django.db.models import Exists, OuterRef, Value, Case, When, Q
+from django.db.models.fields import CharField
 from flowback.group.models import GroupThread
 
 
@@ -10,10 +11,14 @@ def reports_list(fetched_by: User):
     if not (fetched_by.is_staff or fetched_by.is_superuser):
         raise PermissionDenied('Only server staff members can view reports')
 
-    print(list(Poll.objects.filter(id__in=Report.objects.values_list('post_id', flat=True))), "QUERY")
-
     return Report.objects.all().order_by('-created_at').annotate(
         admin_action=Case(
-            When(Exists(Poll.objects.filter(id=OuterRef('post_id'), active=True)), then=Value("nothing")),
-            default=Value("deleted")
-        ))
+            When(
+                Q(Exists(Poll.objects.filter(id=OuterRef('post_id'), active=True)))
+                & Q(Exists(GroupThread.objects.filter(id=OuterRef('post_id'), active=True))),
+                then=Value("nothing")
+            ),
+            default=Value("deleted"),
+            output_field=CharField()
+        )
+    )
