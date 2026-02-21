3f7003d28b9fa417cc83f113dab0bbf42f67a497
emil <53380638+Kattenelvis@users.noreply.github.com>
2026-02-21 14:41:54 +0100

    Finished actions taken by admin

--- flowback/server/selectors.py
+++ flowback/server/selectors.py
@@ -2,7 +2,7 @@ from rest_framework.exceptions import PermissionDenied
 
 from flowback.user.models import User, Report
 from flowback.poll.models import Poll
-from django.db.models import Q, Exists, OuterRef, F, Subquery, Value
+from django.db.models import Q, Exists, OuterRef, F, Subquery, Value, Case, When
 from flowback.group.models import GroupThread
 
 
@@ -10,5 +10,10 @@ def reports_list(fetched_by: User):
     if not (fetched_by.is_staff or fetched_by.is_superuser):
         raise PermissionDenied('Only server staff members can view reports')
 
+    print(list(Poll.objects.filter(id__in=Report.objects.values_list('post_id', flat=True))), "QUERY")
+
     return Report.objects.all().order_by('-created_at').annotate(
-        admin_action=Value("nothing" if Exists(Subquery(Poll.objects.filter(id=OuterRef('post_id')))) else "deleted"))
+        admin_action=Case(
+            When(Exists(Poll.objects.filter(id=OuterRef('post_id'), active=True)), then=Value("nothing")),
+            default=Value("deleted")
+        ))
